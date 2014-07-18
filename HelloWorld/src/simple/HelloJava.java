9daf7b1dfee0380669a75b50a0e994fce0aaa2eb
kimsalt <somna@nate.com>
2014-07-18 14:42:41 +0900

    zomna.tistory.com

---
+++ HelloWorld/src/simple/HelloJava.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		String string = "HelloWorld";
+		print(string);
+	}
+
+	public static void print(String string) {
+		System.out.println(string);
+	}
+}
---
+++ HelloWorld/src/simple/Math.java
@@ -0,0 +1,14 @@
+package simple;
+
+public class Math {
+
+	public static void main(String[] args) {
+		// 2, 3 더하기
+		System.out.println(2 + 3);
+		
+		// 3, 5 더하기
+		System.out.println(3 + 5);
+
+	}
+
+}
